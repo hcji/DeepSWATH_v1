351dbf329778136928e88a8f7c60189d740ed840
hcji <ji.hongchao@foxmail.com>
2020-01-05 10:32:59 +0800

    writable

--- DeepDIA/DeepDIA.py
+++ DeepDIA/DeepDIA.py
@@ -13,24 +13,24 @@ from tensorflow.keras.models import load_model
 from sklearn.preprocessing import normalize
 from DeepDIA.utils import parser_mzxml, extract_eic, fragment_eic, get_ms2
 
-def DeepDIA_process(file, features, noise=200):
+def DeepDIA_process(file, features, output_path, noise=200):
     # file = 'Example/CS52684_neg_SWATH.mzXML'
     # features = pd.read_csv('Example/CS52684_neg_SWATH.features.csv')
+    # output_path = 'Example/CS52684_neg_IDA_SWATH.ms2.csv'
     mod = load_model('Model/DeepDIA_Model.h5')
     peaks = parser_mzxml(file)
-    all_precursor_mz, all_precursor_rt = [], []
-    all_exid, all_frag_mz, all_frag_abund = [], [], []
-    all_precursor_eics, all_fragment_eics = [], []
     
     peaks1 = [p for p in peaks if p.getMSLevel()==1]
     peaks2 = [p for p in peaks if p.getMSLevel()==2]
     precursors = np.unique([p.getPrecursors()[0].getMZ() for p in peaks2])
     del(peaks)
     
-    for i in tqdm(features.index):
-        exid = features.iloc[i, 0]
-        exrt = features['rt'][i]
-        exmz = features['mz'][i]
+    # output = pd.DataFrame(columns = ['exid', 'precursor_mz', 'precursor_rt', 'mz', 'intensity'])
+    output = open (output_path, 'a+')
+    for i in tqdm(range(len(features.index))):
+        exid = features.index[i]
+        exrt = features['rt'][exid]
+        exmz = features['mz'][exid]
         exeic = extract_eic(peaks1, exmz, exrt, rtlength=30)
         # plt.plot(exeic[0], exeic[1])
         
@@ -39,6 +39,10 @@ def DeepDIA_process(file, features, noise=200):
         candidate_mz, candidate_abund = ms2[0][cid], ms2[1][cid]
         if len(candidate_mz) < 1:
             continue
+        
+        temp_frag_mz, temp_frag_abund = [], []
+        temp_precursor_eics, temp_fragment_eics = [], []
+        
         for j in range(len(candidate_mz)):
             fragmz = candidate_mz[j]
             fragabund = candidate_abund[j]
@@ -49,27 +53,29 @@ def DeepDIA_process(file, features, noise=200):
             std_ex = np.interp(std_rt, exeic[0], exeic[1])
             std_fg = np.interp(std_rt, frageic[0], frageic[1])
             
-            all_exid.append(exid)
-            all_precursor_mz.append(exmz)
-            all_precursor_rt.append(exrt)
-            all_frag_mz.append(fragmz)
-            all_frag_abund.append(fragabund)
-            all_precursor_eics.append(std_ex)
-            all_fragment_eics.append(std_fg)
-    X1 = np.asarray(all_precursor_eics)
-    X2 = np.asarray(all_fragment_eics)
+            temp_frag_mz.append(fragmz)
+            temp_frag_abund.append(fragabund)
+            temp_precursor_eics.append(std_ex)
+            temp_fragment_eics.append(std_fg)
+        
+        X1 = np.asarray(temp_precursor_eics)
+        X2 = np.asarray(temp_fragment_eics)
     
-    X1 = normalize(X1, axis=1, norm='max')
-    X2 = normalize(X2, axis=1, norm='max')
-    X1 = np.expand_dims(X1,-1)
-    X2 = np.expand_dims(X2,-1)
+        X1 = normalize(X1, axis=1, norm='max')
+        X2 = normalize(X2, axis=1, norm='max')
+        X1 = np.expand_dims(X1,-1)
+        X2 = np.expand_dims(X2,-1)
     
-    prediction = mod.predict([X1, X2])
-    pos = np.where(prediction[:,1] > 0.5)[0]
-    # plt.plot(all_precursor_eics[pos[21]])
-    # plt.plot(all_fragment_eics[pos[21]])
-    output = pd.DataFrame({'exid': np.asarray(all_exid)[pos], 'precursor_mz': np.asarray(all_precursor_mz)[pos], 
-                           'precursor_rt': np.asarray(all_precursor_rt)[pos], 'mz': np.asarray(all_frag_mz)[pos],
-                           'intensity': np.asarray(all_frag_abund)[pos]})
-    return output
+        prediction = mod.predict([X1, X2])
+        pos = np.where(prediction[:,1] > 0.5)[0]
+        # plt.plot(all_precursor_eics[pos[21]])
+        # plt.plot(all_fragment_eics[pos[21]])
+        temp_frag_mz = np.asarray(temp_frag_mz)[pos]
+        temp_frag_abund = np.asarray(temp_frag_abund)[pos]
+        temp_output = pd.DataFrame({'exid': exid, 'precursor_mz': exmz, 'precursor_rt': exrt, 
+                                    'mz': temp_frag_mz, 'intensity': temp_frag_abund})
+        # output = output.append(temp_output)
+        output.write(str(temp_output))
+    output.close()
+    return pd.read_csv(output_path, index_col=0)
         
--- Example/scripts.py
+++ Example/scripts.py
@@ -8,10 +8,15 @@ Created on Sat Jan  4 08:04:29 2020
 import pandas as pd
 from DeepDIA.DeepDIA import DeepDIA_process
 
-
+# untargeted
 file = 'Example/CS52684_neg_SWATH.mzXML'
-features = pd.read_csv('Example/CS52684_neg_SWATH.features.csv')
-    
+features = pd.read_csv('Example/CS52684_neg_SWATH.features.csv', index_col=0)    
 dia_result = DeepDIA_process(file, features)
 dia_result.to_csv('Example/CS52684_neg_SWATH.ms2.csv')
+
+# targeted
 dda_result = pd.read_csv('Example/CS52684_neg_IDA.ms2.csv')
+features = dda_result[['precursor_mz', 'precursor_rt']]
+features = features.drop_duplicates()
+features.columns = ['mz', 'rt']
+dia_result = DeepDIA_process(file, features)
