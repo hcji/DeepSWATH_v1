857ccdb88bb6caa70e7f697d558a5ec694a88373
hcji <ji.hongchao@foxmail.com>
2020-01-09 07:56:36 +0800

    MetaboDIA_Data DeepDIA-process

--- Comparision/MetaboDIA_Data/DeepDIA-process.py
+++ Comparision/MetaboDIA_Data/DeepDIA-process.py
@@ -9,15 +9,16 @@ import os
 import pandas as pd
 from DeepDIA.DeepDIA import DeepDIA_process
 
-Dir = 'E:/project/MetDIA demo data/data'
+Dir = 'D:/MetaboDIA_data/PH'
 files = os.listdir(Dir)
-files = [f for f in files if '.mzXML' in f]
+files = [f for f in files if '_SWATH.mzXML' in f]
 for f in files:
     f = Dir + '/' + f
     ft, fo = f, f
-    dda_result = pd.read_csv(ft.replace('.mzXML', '.ms2.csv'))
-    features = dda_result[['mz', 'rt', 'maxo']]
+    dda_result = pd.read_csv(ft.replace('_SWATH.mzXML', '_IDA.ms2.csv'))
+    features = dda_result[['precursor_mz', 'precursor_rt', 'precursor_intensity']]
+    features = features.drop_duplicates()
     features.columns = ['mz', 'rt', 'intensity']
     dia_result = DeepDIA_process(f, features)
-    dia_result.to_csv(fo.replace('.mzXML', '.deepdia.csv'))
+    dia_result.to_csv(fo.replace('.mzXML', '.ms2.csv'))
     
