0be62958f80d21140200c5c8ac15df715a104ded
hcji <ji.hongchao@foxmail.com>
2020-01-08 16:09:46 +0800

    include precursor intensity

--- Comparision/MS-DIAL.py
+++ Comparision/MS-DIAL.py
@@ -15,10 +15,10 @@ def parser_msdial(file, output):
     for (data, spectrum) in parser:
         exmz += [data['precursormz']] * spectrum.shape[0]
         exrt += [data['retentiontime'] * 60] * spectrum.shape[0]
-        exint += [data['intensity'] * spectrum.shape[0]]
+        exint += [data['intensity']] * spectrum.shape[0]
         mz += list(spectrum[:,0])
         intensity += list(spectrum[:,1])
-    res = pd.DataFrame({'precursor_mz':exmz, 'precursor_rt':exrt, 'precursor_intensity':intensity,
+    res = pd.DataFrame({'precursor_mz':exmz, 'precursor_rt':exrt, 'precursor_intensity':exint,
                            'mz': mz, 'intensity': intensity})
     res.to_csv(output, index=False)
     return res
