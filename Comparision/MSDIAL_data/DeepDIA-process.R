4e1cf1f52609bb0e5864b2dadbcf2e8e46932e10
hcji <ji.hongchao@foxmail.com>
2020-01-17 14:06:57 +0800

    MSDIAL data

---
+++ Comparision/MSDIAL_data/DeepDIA-process.R
@@ -0,0 +1,14 @@
+# xcms version 3.8.1
+library(xcms)
+
+get_ms1_features <- function(file, snthresh = 10, prefilter = c(3, 100), noise = 200, ppm = 30, peakwidth = c(5, 60)){
+  data <- readMSData(file, mode = "onDisk") 
+  cwp <- CentWaveParam(snthresh = snthresh, noise = noise, prefilter = prefilter, ppm = ppm, peakwidth = peakwidth)
+  data <- findChromPeaks(data, param = cwp)
+  peaks <- chromPeaks(data)
+  return(peaks)
+}
+
+file <- 'E:/project/MSDIAL_data/20140809_MSDIAL_DemoFiles_Swath (wiff.wiffscan)/Nega_Swath_QC_1_1.mzML'
+features = get_ms1_features(file, prefilter = c(1, 1000))
+write.csv(features, stringr::str_replace(file, '.mzML', '.feature.csv'))
